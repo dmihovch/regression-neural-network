55d47cf5e1fdb7654c517ca5a56d6a939ff3e585
dmihovch <dmihovch@gmail.com>
2025-05-10 21:09:48 -0400

    some poking and proding

--- include/debug.h
+++ include/debug.h
@@ -7,3 +7,4 @@ void matrix_print_rowcol(double* arr, int size,int flag);
 void matrix_print(const matrix_t *m);
 void matrix_set_linear_range(matrix_t* m);
 void matrix_set_rand_val(matrix_t* m);
+void matrix_set_const_val(matrix_t* m, double x);
--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ int main(/*int argc, char* argv[]*/){
 
     // Set up an input matrix: 2 samples, 3 features each
     int layer_sizes[5] = {25,75,150,15,1};
-    model_t* model = model_init(50, layer_sizes,A_RELU, 5, 1.1);
+    model_t* model = model_init(50, layer_sizes,A_RELU, 5, 0);
     for(int i = 0; i<5;++i){
         if(i == 0){
 
@@ -28,6 +28,18 @@ int main(/*int argc, char* argv[]*/){
         }
 
     }
+
+    matrix_t* y_true = matrix_alloc(100, 1);
+    if(y_true == NULL){
+        model_free(model);
+        return 1;
+    }
+
+    matrix_set_const_val(y_true, 1.0);
+    double mse = mean_squared_error_loss(y_true, model->layers[4]->output);
+
+    printf("mse: %0.5f\n",mse);
+    matrix_free(y_true);
     model_free(model);
     return 0;
 }
