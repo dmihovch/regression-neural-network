c3ea378a0e8aceb1be622932fe674a800d9a5541
Dan Mihovch <dmihovch@gmail.com>
2025-12-23 14:29:05 -0500

    i want to work on this again

--- src/main.c
+++ src/main.c
@@ -11,15 +11,15 @@
 int main(/*int argc, char* argv[]*/){
     seedlcgrandf(time(NULL));
 
-    matrix_t* init = matrix_alloc(10, 10);
+    matrix_t* init = matrix_alloc(1000, 1000);
     if(init == NULL){
         return 1;
     }
     matrix_set_rand_val(init);
 
-    int layer_sizes[5] = {10,7,15,10,1};
-    model_t* model = model_init(10, layer_sizes,A_RELU, 5, 0.01);
-    matrix_t* y_true = matrix_alloc(10, 1);
+    int layer_sizes[5] = {100,70,150,100,1};
+    model_t* model = model_init(1000, layer_sizes,A_RELU, 5, 0.01);
+    matrix_t* y_true = matrix_alloc(1000, 1);
     if(y_true == NULL){
         model_free(model);
         return 1;
@@ -27,7 +27,7 @@ int main(/*int argc, char* argv[]*/){
     matrix_set_const_val(y_true, 1.0);
 
 
-    model_train(model, init, y_true, 10);
+    model_train(model, init, y_true, 1000);
     matrix_free(init);
     matrix_free(y_true);
     model_free(model);
